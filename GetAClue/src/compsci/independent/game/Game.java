b19d095e1f255d3182173f65dee07e8cf2daf90c
Sam Davis <samdavis16e@gmail.com>
2015-10-09 09:45:09 -0400

    yeah thats a thing

---
+++ GetAClue/src/compsci/independent/game/Game.java
@@ -0,0 +1,35 @@
+package compsci.independent.game;
+
+import java.util.Scanner;
+
+import compsci.independent.Player;
+
+public class Game {
+	
+		public void runGame(){
+			int handSize;
+			String name;
+			Scanner scanner=new Scanner(System.in);
+			
+			System.out.print("How many players?\t");
+			int numPlayers=scanner.nextInt();
+			Player[] players=new Player[numPlayers];
+			scanner.nextLine();
+			
+			for(int i=0; i<numPlayers;){
+				System.out.print("What is Player "+(i+1)+"'s name?\t");
+				name=scanner.nextLine();
+				System.out.print("How many cards does "+name+" have?\t");
+				handSize=scanner.nextInt();
+				players[i]=new Player(name, handSize);
+			}
+			
+			while(true){
+				System.out.print("What was the value of guessed card ");
+			}
+			
+			
+		}
+		
+		
+}
